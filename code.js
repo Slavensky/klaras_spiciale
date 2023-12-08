0222d61133feec801bffaa85845e64a5501f247b
Andreas Slavensky <andreasslavensky@Marens-MacBook-Pro.local>
2023-12-08 17:49:59 +0100

    update

--- code.js
+++ code.js
@@ -7,8 +7,8 @@ function displayFigure() {
     var month = currentDate.getMonth();
     var year = currentDate.getFullYear();
 
-    if (!(dayOfMonth < 15 && month == 11 && year == 2023)) {
-        svar.innerHTML = "Ja!";
+    if (!(dayOfMonth < -1 && month == 11 && year == 2023)) {
+        svar.innerHTML = "JA! TILLYKKE FOR HELVEDE!";
         fig.src = "images/klara_glad.png";
         fig.alt = "Klara er glad";
         console.log(dayOfMonth, month, year);
